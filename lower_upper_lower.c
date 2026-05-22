cf65cff7a4a99710eec2e7f39710b2b36ee7b9e1
mahade <owork.hasan@gmail.com>
2026-05-22 11:40:10 +0600

    commited

---
+++ lower_upper_lower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char x;
+    scanf("%c", &x);
+
+    if (x >= 'a' && x <= 'z')
+    {
+        printf("%c\n", x - 32);
+    }
+    else
+    {
+        printf("%c\n", x + 32);
+    }
+
+    return 0;
+}
